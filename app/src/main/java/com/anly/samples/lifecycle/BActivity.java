b195a241b9463a7e01df76ea785b20ece0fa2605
mingjunli <anly.pear@gmail.com>
2016-07-25 17:01:58 +0800

    update activiy's label

--- app/src/main/java/com/anly/samples/lifecycle/BActivity.java
+++ app/src/main/java/com/anly/samples/lifecycle/BActivity.java
@@ -13,6 +13,7 @@ public class BActivity extends TraceActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+        getSupportActionBar().setTitle("B-Activity");
 
         findViewById(R.id.btn_a).setOnClickListener(this);
         findViewById(R.id.btn_b).setOnClickListener(this);
