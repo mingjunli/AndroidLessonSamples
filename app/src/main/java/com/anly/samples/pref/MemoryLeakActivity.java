9e47f0b308e26f0b397a1d5632a8f85a694ec535
imesong <imesong@126.com>
2016-11-07 17:31:16 +0800

    添加内存泄漏日志，修复内存泄漏方法

--- app/src/main/java/com/anly/samples/pref/MemoryLeakActivity.java
+++ app/src/main/java/com/anly/samples/pref/MemoryLeakActivity.java
@@ -1,9 +1,10 @@
 package com.anly.samples.pref;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.anly.samples.R;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MemoryLeakActivity extends AppCompatActivity implements SampleListener {
 
@@ -13,12 +14,21 @@ public class MemoryLeakActivity extends AppCompatActivity implements SampleListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_leak);
-
+        Log.d("MemoryLeakActivity","onCreat size:"+ListenerManager.getInstance().getListenerSize());
         ListenerManager.getInstance().addListener(this);
     }
 
     @Override
     public void doSomething() {
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        int size = ListenerManager.getInstance().getListenerSize();
+        Log.d("MemoryLeakActivity","size:"+size);
+        //TODO 修复内存泄漏方法
+        //ListenerManager.getInstance().removeListener(this);
     }
 }
